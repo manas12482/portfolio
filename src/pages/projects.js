539b62e5172928e4bf4a9feffffdc0e0eeb19210
mishra-suyash <mishrasuyash@hotmail.com>
2023-08-17 14:42:32 +0530

    Projects Page Completed !RESPONSIVE

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,6 +2,109 @@ import React from 'react';
 import Head from 'next/head';
 import Layout from '@/components/Layout';
 import AnimatedText from '@/components/AnimatedText';
+import Link from 'next/link';
+import Image from 'next/image';
+import { GithubIcon } from '@/components/icons';
+import { motion } from 'framer-motion';
+import project1 from '../../public/images/projects/portfolio-cover-image.jpg';
+import articles from './articles';
+
+const FeaturedProject = ({
+	title,
+	type,
+	description,
+	image,
+	link,
+	githubLink,
+	summary,
+}) => {
+	return (
+		<article className='p-12  w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light relative rounded-br-2xl'>
+			<div className='absolute top-0 left-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl' />
+			<Link
+				href={link}
+				target={'_blank'}
+				className='w-1/2 cursor-pointer overflow-hidden rounded-lg '
+			>
+				<Image src={image} alt={title} className='w-full h-auto' />
+			</Link>
+
+			<div className='w-1/2 flex flex-col items-start justify-between pl-6'>
+				<span className='text-primary font-medium text-xl '>{type}</span>
+				<Link href={link} target={'_blank'}>
+					<h2 className='my-2 w-full text-left text-4xl font-bold hover:underline underline-offset-2'>
+						{title}
+					</h2>
+				</Link>
+				<p className='my-2 font-medium text-dark '>{description}</p>
+				<div className='mt-2 flex items-center '>
+					{githubLink ? (
+						<Link href={githubLink} target={'_blank'} className='w-10 '>
+							<GithubIcon />
+						</Link>
+					) : (
+						''
+					)}
+					{link ? (
+						<Link
+							href={link}
+							target={'_blank'}
+							className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold'
+						>
+							Visit Project
+						</Link>
+					) : (
+						''
+					)}
+				</div>
+			</div>
+		</article>
+	);
+};
+
+const Project = ({ title, type, image, link, githubLink }) => {
+	return (
+		<article className='w-full flex flex-col items-center rounded-2xl border border-solid border-dark bg-light p-6 relative '>
+			<div className='absolute top-0 left-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl' />
+			<Link
+				href={link}
+				target={'_blank'}
+				className='w-full cursor-pointer overflow-hidden rounded-lg '
+			>
+				<Image src={image} alt={title} className='w-full h-auto' />
+			</Link>
+
+			<div className='w-full flex flex-col items-start justify-between pl-6'>
+				<span className='text-primary font-medium text-xl '>{type}</span>
+				<Link href={link} target={'_blank'}>
+					<h2 className='my-2 w-full text-left text-3xl font-bold hover:underline underline-offset-2'>
+						{title}
+					</h2>
+				</Link>
+				<div className='w-full mt-2 flex items-center justify-between'>
+					{link ? (
+						<Link
+							href={link}
+							target={'_blank'}
+							className='text-dark text-lg font-semibold hover:underline'
+						>
+							Visit
+						</Link>
+					) : (
+						''
+					)}
+					{githubLink ? (
+						<Link href={githubLink} target={'_blank'} className='w-8 '>
+							<GithubIcon />
+						</Link>
+					) : (
+						''
+					)}
+				</div>
+			</div>
+		</article>
+	);
+};
 
 const projects = () => {
 	return (
@@ -14,8 +117,74 @@ const projects = () => {
 			</Head>
 			<main className='w-full mb-16 flex flex-col items-center justify-center '>
 				<Layout className='pt-16'>
-					<AnimatedText text='Imagination Trumps Knowledge!' />
-					<div className='grid grid-col-12 gap-24'></div>
+					<AnimatedText
+						text='Imagination Trumps Knowledge! '
+						className='mb-16'
+					/>
+					<div className='grid grid-col-12 gap-24 gap-y-32'>
+						<div className='col-span-12'>
+							<FeaturedProject
+								title='React Portfolio Website'
+								type='Featured Project'
+								description='A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth page transitions, cool background effects, unique design and it is mobile responsive.'
+								link='https://mishrasuyash.vercel.app/'
+								githubLink='https://github.com/mishra-suyash/portfolio-nextjs'
+								image={project1}
+							/>
+						</div>
+						<div className='col-span-6'>
+							<Project
+								title='React Portfolio Website'
+								type='Featured Project'
+								description='A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth page transitions, cool background effects, unique design and it is mobile responsive.'
+								link='https://mishrasuyash.vercel.app/'
+								githubLink='https://github.com/mishra-suyash/portfolio-nextjs'
+								image={project1}
+							/>
+						</div>
+						<div className='col-span-6'>
+							<Project
+								title='React Portfolio Website'
+								type='Featured Project'
+								description='A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth page transitions, cool background effects, unique design and it is mobile responsive.'
+								link='https://mishrasuyash.vercel.app/'
+								githubLink='https://github.com/mishra-suyash/portfolio-nextjs'
+								image={project1}
+							/>
+						</div>
+					</div>
+					<div className='grid grid-col-12 gap-24 mt-32 gap-y-32'>
+						<div className='col-span-12'>
+							<FeaturedProject
+								title='React Portfolio Website'
+								type='Featured Project'
+								description='A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth page transitions, cool background effects, unique design and it is mobile responsive.'
+								link='https://mishrasuyash.vercel.app/'
+								githubLink='https://github.com/mishra-suyash/portfolio-nextjs'
+								image={project1}
+							/>
+						</div>
+						<div className='col-span-6'>
+							<Project
+								title='React Portfolio Website'
+								type='Featured Project'
+								description='A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth page transitions, cool background effects, unique design and it is mobile responsive.'
+								link='https://mishrasuyash.vercel.app/'
+								githubLink='https://github.com/mishra-suyash/portfolio-nextjs'
+								image={project1}
+							/>
+						</div>
+						<div className='col-span-6'>
+							<Project
+								title='React Portfolio Website'
+								type='Featured Project'
+								description='A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth page transitions, cool background effects, unique design and it is mobile responsive.'
+								link='https://mishrasuyash.vercel.app/'
+								githubLink='https://github.com/mishra-suyash/portfolio-nextjs'
+								image={project1}
+							/>
+						</div>
+					</div>
 				</Layout>
 			</main>
 		</>
