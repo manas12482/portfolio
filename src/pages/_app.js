0ac53ec783979f0cfec40bb89c79cc4397cf5915
mishra-suyash <mishrasuyash@hotmail.com>
2023-08-18 08:40:09 +0530

    Some more padding fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }) {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main className={`${montserrat.variable} font-mont bg-light dark:bg-dark p-4`}>
+			<main className={`${montserrat.variable} font-mont bg-light dark:bg-dark px-[2vw]`}>
         <Navbar />
 				<Component {...pageProps} />
         <Footer />
